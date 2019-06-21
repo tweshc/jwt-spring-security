570b8db264b86d6f86fa92aa1712c5163a3ae246
Twesh <tweshc@gmail.com>
2019-06-20 21:57:37 -0400

    completed request filter class

--- src/main/java/com/example/jwt/jwtsecurity/config/JwtUserDetailsService.java
+++ src/main/java/com/example/jwt/jwtsecurity/config/JwtUserDetailsService.java
@@ -11,6 +11,7 @@ public class JwtUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         if ("javainuse".equals(username)) {
+            //hardcoding here instead of calling database
             return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
                     new ArrayList<>());
         } else {
